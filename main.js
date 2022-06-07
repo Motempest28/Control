f15ff7d784a9a07cf97ba80816a676c8e3b5612c
Motempest28 <45185793+Motempest28@users.noreply.github.com>
2022-06-07 11:35:53 -0700

    Github sound testing
    troubleshooting no sound from the github version of the site

--- main.js
+++ main.js
@@ -3,15 +3,16 @@
 //sound import and useage reference
 //importing - https://www.youtube.com/watch?v=hn7MhPt24L4
 //usage - https://github.com/goldfire/howler.js/blob/master/README.md#quick-start
-var sound = new Howl({
-    src: ['/you.mp3']
-  });
+// var sound = new Howl({
+//     src: ['/you.mp3']
+//   });
 
   var sound2 = new Howl({
     src: ['/sounds/you.mp3']
   });
   function playMe(){
-      sound2.play();
+      sound2.play()
+      html5: true
   }
 
 // document.getElementById("demo2").onclick = function(){alert("Hello World!")};
