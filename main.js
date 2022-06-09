bfa142a609fb4b4e9cd0b0cbd10f07327649fae0
Motempest28 <45185793+Motempest28@users.noreply.github.com>
2022-06-08 19:04:06 -0700

    Testing volume controls
    Learning how to use a range slider to control the volume settings from howler

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-//****************Sounds***************************
+//****************Testing***************************
 //sound import and useage reference
 //importing - https://www.youtube.com/watch?v=hn7MhPt24L4
 //usage - https://github.com/goldfire/howler.js/blob/master/README.md#quick-start
@@ -7,18 +7,46 @@
 //     src: ['/you.mp3']
 //   });
 
+
+
+
+function getVolume(){
+    var x = document.getElementById("soundVolume").value;
+    document.getElementById("demo2").innerHTML = x;
+    x = x / 100;
+    alert(x);
+    sound2.volume(x);
+}
+
+
 var sound2 = new Howl({
-    src: ["sounds/you.mp3", "sounds/you.ogg"]
+    src: ["sounds/you.mp3", "sounds/you.ogg"],
+    html5: true,
+    volume: 0.5,
   });
-  function playMe(){
-      sound2.play()
-      html5: true
+
+
+function playMe(){
+      sound2.play();
       document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+      
   }
 
 // document.getElementById("demo2").onclick = function(){alert("Hello World!")};
 document.getElementById("demo").onclick = function() {playMe()};
+document.getElementById("soundcheck").onclick = function() {getVolume()};
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+
+//input field testing
+document.getElementById("myButton").onclick = function(){
+    var userText = document.getElementById("myText").value;
+    console.log(userText);
+    userText = document.getElementById("myText").value = "";
+    console.log(userText);
 }
+
+//*******************************Real Code************************************* */
+
+//Create variables to track items
+
+//let volume = 
